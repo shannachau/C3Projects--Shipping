648034e4516bab947d52955a26ce1b2adff9f6c6
Shanna Chau <shannachau@gmail.com>
2015-08-18 17:09:26 -0700

    Added TODO note as reminder to go back and account for max weight

--- app/controllers/usps_shipments_controller.rb
+++ app/controllers/usps_shipments_controller.rb
@@ -1,4 +1,6 @@
 class UspsShipmentsController < ApplicationController
+  #TODO: Max weight for package is 70. Remember to handle for this
+
   def estimate
     response = usps_login.find_rates(origin, estimate_destination, package)
     usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
