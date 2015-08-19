98c19f74dc2ab188d0afb0ad053b92b4d76c14d2
Shanna Chau <shannachau@gmail.com>
2015-08-18 17:21:02 -0700

    Implemented estimate method for UPS

--- app/controllers/ups_shipments_controller.rb
+++ app/controllers/ups_shipments_controller.rb
@@ -1,2 +1,14 @@
 class UpsShipmentsController < ApplicationController
+  def estimate
+    response = ups_login.find_rates(origin, estimate_destination, package)
+    ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+
+    render json: ups_rates.as_json
+  end
+
+  private
+
+  def ups_login
+    ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY'], origin_name: 'petsy', origin_account: ENV['UPS_ACCOUNT_NO'])
+  end
 end
