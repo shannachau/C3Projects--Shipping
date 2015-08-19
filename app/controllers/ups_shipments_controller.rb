f46d7c8b935f680c76e4eed24ce899f0e142bb10
Shanna Chau <shannachau@gmail.com>
2015-08-19 10:06:36 -0700

    Handled for bad requests

--- app/controllers/ups_shipments_controller.rb
+++ app/controllers/ups_shipments_controller.rb
@@ -1,9 +1,17 @@
 class UpsShipmentsController < ApplicationController
   def estimate
-    response = ups_login.find_rates(origin, estimate_destination, package)
-    ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    if (/\A\d+\z/.match(params[:weight])).nil? || (/\A\d+\z/.match(params[:zip])).nil?
+      render json: { error: "Please enter a numeric value for weight and zip code."}, status: :bad_request
+    elsif params[:weight].to_i > 2400
+      render json: { error: "UPS does not support weights above 150lbs/2400oz." }, status: :bad_request
+    elsif params[:zip].length != 5
+      render json: { error: "Zip code needs to be five digits." }, status: :bad_request
+    else
+      response = ups_login.find_rates(origin, estimate_destination, package)
+      ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
-    render json: ups_rates.as_json
+      render json: ups_rates.as_json
+    end
   end
 
   private
--- app/controllers/usps_shipments_controller.rb
+++ app/controllers/usps_shipments_controller.rb
@@ -2,10 +2,18 @@ class UspsShipmentsController < ApplicationController
   #TODO: Max weight for package is 70. Remember to handle for this
 
   def estimate
-    response = usps_login.find_rates(origin, estimate_destination, package)
-    usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    if (/\A\d+\z/.match(params[:weight])).nil? || (/\A\d+\z/.match(params[:zip])).nil?
+      render json: { error: "Please enter a numeric value for weight and zip code."}, status: :bad_request
+    elsif params[:weight].to_i > 1120
+      render json: { error: "USPS does not support weights above 70lbs/1120oz."} , status: :bad_request
+    elsif params[:zip].length != 5
+      render json: { error: "Zip code needs to be five digits." }, status: :bad_request
+    else
+      response = usps_login.find_rates(origin, estimate_destination, package)
+      usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
-    render json: usps_rates.as_json
+      render json: usps_rates.as_json
+    end
   end
 
   private
