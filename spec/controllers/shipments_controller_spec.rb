609d40376e9c8af9f34bc617b8182d3be88e3b58
Shanna Chau <shannachau@gmail.com>
2015-08-20 16:55:23 -0700

    Testing WIP

--- spec/controllers/shipments_controller_spec.rb
+++ spec/controllers/shipments_controller_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
+require 'support/vcr_setup'
+
 
 RSpec.describe ShipmentsController, type: :controller do
+  describe "GET #estimate" do
+    before :each do
+      VCR.use_cassette 'ship_response' do
+        post :ship, { address1: "123 Fake St",
+                      city:    "Fake City",
+                      state:   "WA",
+                      zip:     "98155",
+                      country: "US"}.to_json,
+                    { format: :json }
+      end
+    end
 
+    context "valid request" do
+      it "is successful" do
+        expect(response.response_code).to eq 200
+      end
+
+      it "returns json" do
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
+
+      context "the returned json object" do
+        before :each do
+          @response = JSON.parse response.body
+        end
+
+        it "is a hash of shipping information" do
+          expect(@response).to be_an_instance_of Hash
+          expect(@response.length).to eq 2
+        end
+
+        it "includes UPS and USPS keys" do
+          expect(@response.keys).to eq(["ups", "usps"])
+        end
+      end
+    end
+
+
+    context "invalid request" do
+    end
+  end
 end
